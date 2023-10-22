adfcc36736f9b330f632cc2eeb0bad1865a64ae7
jeje <57799486+jejehye@users.noreply.github.com>
2023-10-22 23:29:29 +0900

    과제중 4 자동생성추가

--- src/main/java/com/example/BackendPro/MemberEntity.java
+++ src/main/java/com/example/BackendPro/MemberEntity.java
@@ -3,10 +3,7 @@ package com.example.BackendPro;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -14,7 +11,8 @@ import javax.persistence.Table;
 @Table(name = "member")
 public class MemberEntity {
 
-    @Id()//자동생ㅡ
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//자동생성
     private String Id;
     @Column (length = 255, nullable = false)
     private String name;
